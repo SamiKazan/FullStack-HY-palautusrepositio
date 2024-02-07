53164719838e327a00482c0643c29e15ad0cd172
SamiKazan <sami.kazan@helsinki.fi>
2024-02-07 17:12:15 +0200

    tehty tehtävään 2.17 asti

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,46 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import noteServices from './services/people'
+import './index.css'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id:'Arto Hellas', number:'+6471826445'}
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-
-  const addName = (event) => {
-    event.preventDefault()
-
-    const nameObject = {
-      name: newName,
-      number: newNumber
-    }
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
-      setNewName('')
-    }
-    if (persons.some((person) => person.number === newNumber)) {
-      alert(`The number ${newNumber} is already added to phonebook`)
-      setNewNumber('')
-    }
-    else {
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-    setNewNumber('')
-    }
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
+const ShowNames = ({namesToShow, handleRemove}) => {
   return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+    <ul>
+        {namesToShow.map(person => 
+          <li key={person.name}>
+            {person.name} {person.number}
+            <button onClick={() => handleRemove(person.id)}>delete</button>
+          </li>)}
+    </ul>
+  )
+}
+
+
+const RenderNameInput = ({addName, handleNameChange, handleNumberChange, newName, newNumber}) => {
+  return (
+    <form onSubmit={addName}>
         <div>
           name: <input 
           value={newName}
@@ -57,11 +34,152 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+  )
+}
+
+const RenderFilter = ({filters, handleFilterChange}) => {
+  return (
+    <form>
+        <div>
+          Filter name<input
+          value={filters}
+          onChange={handleFilterChange}
+          />
+        </div>
+      </form>
+  )
+}
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  const type = message.includes('Error') 
+  ? 'error' 
+  : 'message'
+
+  return (
+    <div className={type}>
+      {message}
+    </div>
+  )
+}
+
+
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filters, setFilters] = useState('')
+  const [message, setMessage] = useState(null)
+
+  useEffect(() => {
+    noteServices
+      .getAll()
+      .then(returnPerson => {
+        setPersons(returnPerson)
+      })
+  },[])
+  
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      number: newNumber
+    }
+    const currentPerson = persons.find(person => person.name === newName)
+    if (persons.some((person) => person.name === newName)) {
+      if (window.confirm(`${currentPerson.name} is already in phonebook, replace old number with new one?`)){
+        
+        const updatedPerson = { ...currentPerson, number: newNumber }
+        noteServices
+          .update(currentPerson.id, updatedPerson)
+
+          .then(response => {
+            setPersons(persons.map(person => person.id !== currentPerson.id
+              ? person
+              : response))
+              setMessage(`Changed number of ${currentPerson.name}`)
+              setTimeout(() => {
+                  setMessage(null)
+              }, 5000)
+          })
+          .catch(error => {
+            setMessage(`Error: Info of ${currentPerson.name} has already been removed`)
+              setTimeout(() => {
+                  setMessage(null)
+              }, 5000)
+          })
+      }
+      
+      setNewName('')
+      setNewNumber('')
+    }
+    else {
+      noteServices
+      .add(nameObject)
+        .then(returnPerson => {
+          setPersons(persons.concat(returnPerson))
+          setNewName('')
+          setNewNumber('')
+          setMessage(`Added ${newName}`)
+          setTimeout(() => {
+              setMessage(null)
+          }, 5000)
+      })
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilters(event.target.value)
+  }
+
+  const handleRemove = (id) => {
+    const currentName = persons.find(person => person.id === id)
+    if (window.confirm(`Do you want to remove this number?`)) {
+      noteServices
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+          setMessage(`Removed ${currentName.name}`)
+          setTimeout(() => {
+              setMessage(null)
+          }, 5000)
+        })
+    }
+  }
+
+  const namesToShow = filters
+  ? persons.filter(person => person.name.toUpperCase().includes(filters.toUpperCase()))
+  : persons;
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={message}/>
+      <RenderFilter
+      filters={filters}
+      handleFilterChange={handleFilterChange}/>
+
+      <h2>Add new</h2>
+      <RenderNameInput
+      newName={newName}
+      newNumber={newNumber}
+      addName={addName}
+      handleNameChange={handleNameChange}
+      handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => 
-          <li key={person.name}>{person.name} {person.number}</li>)}
-      </ul>
+      <ShowNames namesToShow={namesToShow} handleRemove={handleRemove}/>
       ...
     </div>
   )
