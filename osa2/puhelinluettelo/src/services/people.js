38a6354b716d49845b50ddd483240efb96d0a564
SamiKazan <sami.kazan@helsinki.fi>
2024-02-12 15:23:03 +0200

    muutettu toimimaan backendin kanssa

--- osa2/puhelinluettelo/src/services/people.js
+++ osa2/puhelinluettelo/src/services/people.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 
 const getAll = () => {
@@ -20,6 +20,6 @@ const remove = (id) => {
 const update = (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)
-  }
+}
 
 export default { getAll, add, remove, update }
